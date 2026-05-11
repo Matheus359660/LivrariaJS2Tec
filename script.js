8740c7b08e72f7e636ba067cab98e8f63d6596fd
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2026-05-11 17:39:59 +0000

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+let precoLivros = [25, 15, 50, 110, 160, 78, 32];
+
+let maisBarato = 0;
+
+for (let atual = 0; atual < precoLivros.length; atual++){
+    if (precoLivros[atual] < precoLivros[maisBarato]){
+
+    }
+}
+
+console.log(`O livro mais barato custa: ${precoLivros[maisBarato]} mangos`);
