7eefcd5ea027cab7ad59f822d8caef7143ff9839
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2026-05-13 13:00:10 +0000

    OK

---
+++ listaLivro.js
@@ -0,0 +1,44 @@
+let precoLivros = [
+    {
+        titulo: "percy jackson",
+        preco: 50
+    },
+    {
+        titulo: "Quatro vidas de um cachorro",
+        preco: 150
+    },
+    {
+        titulo: "A caçadora de arvores",
+        preco: 210
+    },
+    {
+        titulo: "O Mágico de Oz",
+        preco: 89
+    },
+    {
+        titulo: "Harry Potter e o prisioneiro de Azkaban",
+        preco: 85
+    },
+    {
+        titulo: "Harry Potter e a Pedra Filosofal",
+        preco: 150
+    },
+    {
+        titulo: "O pequeno príncipe",
+        preco:87
+    },
+    {
+        titulo: "Harry Potter e a Câmara Secreta",
+        preco: 99
+    },
+    {
+        titulo: "Eu, Robô",
+        preco: 57
+    },
+    {
+        titulo: "Frankenstein",
+        preco: 100
+    }
+]
+
+module.exports = precoLivros;
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let precoLivros = [25, 15, 50, 110, 160, 78, 32];
-
+let livros = require('./listaLivro')
 let maisBarato = 0;
 
-for (let atual = 0; atual < precoLivros.length; atual++){
-    if (precoLivros[atual] < precoLivros[maisBarato]){
-
+for (let atual = 0; atual < livros.length; atual++){
+    if (livros[atual].preco < livros[maisBarato].preco){
+        maisBarato = atual;
     }
 }
 
-console.log(`O livro mais barato custa: ${precoLivros[maisBarato]} mangos`);
+console.log(`O livro mais barato custa: ${livros[maisBarato].preco} mangos e se chama ${livros[maisBarato].titulo}`);
